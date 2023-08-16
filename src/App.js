904dfd1b6ddb22a87ec146f16ad31a03fc1ae351
igos56 <igosrus56@gmail.com>
2023-08-16 22:47:35 +0500

    Add TodosList component and import in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import API from "./api";
+import TodosList from "./components/todosList/todosList";
 
 const App = () => {
     // пробуем вывести в консоль ответ с api
     API.todos.fetchAll().then((todosData) => console.log(todosData));
 
-    return <div className="App">{<h1>App test - content</h1>}</div>;
+    return (
+        <div className="App">
+            <TodosList />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/todosList/todosList.jsx
@@ -0,0 +1,5 @@
+const TodosList = () => {
+    return <h2>todosList Content</h2>;
+};
+
+export default TodosList;
